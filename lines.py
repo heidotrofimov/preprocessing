0525417eb506bb465467948d49db3595f2e47a44
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-03-18 16:27:22 +0200

    Update lines.py

--- lines.py
+++ lines.py
@@ -1,10 +1,16 @@
 import os
 
-for S2_product in os.listdir('/home/heido/projects/heido_test/s2_zip'):
-  for filename in os.listdir('/home/heido/projects/heido_test/s2_zip/'+S2_product+'/GRANULE/'):
-    if("L2" in filename):
-      line="/snap/snap8/bin/gpt Write -Ssource=\'/home/heido/projects/heido_test/s2_zip/"+S2_product+"/GRANULE/"+filename+"/\' -Pfile=\'/home/heido/projects/heido_test/s2_zip/"+S2_product+"/GRANULE/output.dim"
-      print(line)
+
+if(sys.argv[1]=="S2"):
+  f=open("lines_for_gpt.txt","w")
+  for S2_product in os.listdir('/home/heido/projects/heido_test/s2_zip'):
+    for filename in os.listdir('/home/heido/projects/heido_test/s2_zip/'+S2_product+'/GRANULE/'):
+      if("L2" in filename):
+        if(os.path.exists("/home/heido/projects/heido_test/s2_zip/"+S2_product+"/GRANULE/output.dim")==False):
+          line="/snap/snap8/bin/gpt Write -Ssource=\'/home/heido/projects/heido_test/s2_zip/"+S2_product+"/GRANULE/"+filename+"/\' -Pfile=\'/home/heido/projects/heido_test/s2_zip/"+S2_product+"/GRANULE/output.dim"
+          f.write(line)
+        
+  f.close()
   
 
 
