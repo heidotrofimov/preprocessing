546eed4e743c9b3f1c0ddbb0f3d6f1b51fe41b89
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-03-18 11:39:40 +0200

    Update process.py

--- process.py
+++ process.py
@@ -11,6 +11,8 @@ from snappy import ProductData
 from PIL import Image
 from snappy import ProductUtils
 from snappy import ProgressMonitor
+from snappy import Product
+from snappy import FlagCoding
 
 jpy = snappy.jpy
 ImageManager = jpy.get_type('org.esa.snap.core.image.ImageManager')
@@ -24,21 +26,30 @@ height = product.getSceneRasterHeight()
 VH = product.getBand('Amplitude_VH_S')
 VV = product.getBand('Amplitude_VV_S')
 
-VHVVBand = product.addBand('VHVV', ProductData.TYPE_FLOAT32)
-writer = ProductIO.getProductWriter('BEAM-DIMAP')
-product.setProductWriter(writer)
+#Get new band, Vh+VV
+BandDescriptor = jpy.get_type('org.esa.snap.core.gpf.common.BandMathsOp$BandDescriptor')
 
-rVH = np.zeros(width, dtype=np.float32)
-rVV = np.zeros(width, dtype=np.float32)
+targetBand = BandDescriptor()
+targetBand1.name = 'VHVV'
+targetBand1.type = 'float32'
+targetBand1.expression = 'Amplitude_VH_S + Amplitude_VV_S'
 
-print("Writing...")
+targetBands = jpy.array('org.esa.snap.core.gpf.common.BandMathsOp$BandDescriptor', 2)
+targetBands[0] = targetBand1
 
-for y in range(height):
-    rVH = VH.readPixels(0, y, width, 1, rVH)
-    rVV = VV.readPixels(0, y, width, 1, rVV)
-    rVHVV = rVH + rVV
-    VHVVBand.writePixels(0, y, width, 1, rVHVV)
+parameters = HashMap()
+parameters.put('targetBands', targetBands)
 
+result = GPF.createProduct('BandMaths', parameters, product)
+
+print("Writing new product")
+
+ProductIO.writeProduct(result, 'snappy_bmaths_output.dim', 'BEAM-DIMAP')
+
+
+#New band done
+
+'''
 def write_rgb_image(bands, filename, format):
     image_info = ProductUtils.createImageInfo(bands, True, ProgressMonitor.NULL)
     im = ImageManager.getInstance().createColoredBandImage(bands, image_info, 0)
@@ -48,23 +59,4 @@ red = product.getBand('Amplitude_VH_S')
 green = product.getBand('Amplitude_VV_S')
 blue = product.getBand('VHVVBand')
 write_rgb_image([red, green, blue], 'gamma_export', 'png')
-
-'''
-VHVV=product.getBand('VHVV')
-rgb=np.zeros((height,width,3))
-rgb[...,0]=rVH
-rgb[...,1]=rVV
-rgb[...,2]=rVH
-
-im_result = Image.fromarray(np.uint8(rgb))
-im_result.save("name.png")
-
-w = rad13.getRasterWidth()
-h = rad13.getRasterHeight()
-rad13_data = np.zeros(w * h, np.float32)
-rad13.readPixels(0, 0, w, h, rad13_data)
-p.dispose()
-rad13_data.shape = h, w
-imgplot = plt.imshow(rad13_data)
-imgplot.write_png('radiance_13.png')
 '''
