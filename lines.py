41ea403ba83492fefaaa7773dd290b6004f8d904
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-03-18 17:02:07 +0200

    Update lines.py

--- lines.py
+++ lines.py
@@ -4,9 +4,9 @@ import sys
 if(sys.argv[1]=="S2"):
   f=open("/home/heido/projects/heido_test/preprocessing/lines_for_gpt.txt","w")
   for S2_product in os.listdir('s2_zip'):
-    for filename in os.listdir('s2_zip'+S2_product+'/GRANULE/'):
+    for filename in os.listdir('s2_zip/'+S2_product+'/GRANULE/'):
       if("L2" in filename):
-        if(os.path.exists('s2_zip'+S2_product+"/GRANULE/output.dim")==False):
+        if(os.path.exists('s2_zip/'+S2_product+"/GRANULE/output.dim")==False):
           line="/snap/snap8/bin/gpt Write -Ssource=\"s2_zip/"+S2_product+"/GRANULE/"+filename+"/\" -Pfile=\"s2_zip/"+S2_product+"/GRANULE/output.dim\""
           f.write(line+"\n")
         
