eb4a5f1637825dfc9983a66859f2d02415b8dc39
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-03-18 16:33:18 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 python lines.py S2
-while IFS= read -r line; do
-$line
-done < lines_for_gpt.txt
+#while IFS= read -r line; do
+#$line
+#done < lines_for_gpt.txt
 
-rm lines_for_gpt.txt
+#rm lines_for_gpt.txt
