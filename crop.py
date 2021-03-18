570c9686c4ebe0c4da61af9e9fbaef4351e2f87c
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-03-18 21:14:24 +0200

    Update crop.py

--- crop.py
+++ crop.py
@@ -13,7 +13,7 @@ def crop_up(img):
   for i in range(img_o.width):
     greatest_from_column=0
     for j in range(img_o.height):
-      if(img[j][i][3]==0):
+      if(img[j,i][3]==0):
         greatest_from_column=j
     pixels_to_be_cropped.append(greatest_from_column)
   cropped_img=img_o.crop(0,min(pixels_to_be_cropped),0,0)
@@ -26,7 +26,7 @@ def crop_bottom(img):
   for i in range(img_o.width):
     greatest_from_column=0
     for j in range(img_o.height):
-      if(img[img.height-j][i][3]==0):
+      if(img[img.height-j,i][3]==0):
         greatest_from_column=j
     pixels_to_be_cropped.append(greatest_from_column)
   cropped_img=img_o.crop(0,0,0,min(pixels_to_be_cropped))
@@ -39,7 +39,7 @@ def crop_left(img):
   for i in range(img_o.height):
     greatest_from_row=0
     for j in range(img_o.width):
-      if(img[i][j][3]==0):
+      if(img[i,j][3]==0):
         greatest_from_row=j
     pixels_to_be_cropped.append(greatest_from_row)
   cropped_img=img_o.crop(min(pixels_to_be_cropped),0,0,0)
@@ -52,7 +52,7 @@ def crop_right(img):
   for i in range(img_o.height):
     greatest_from_row=0
     for j in range(img_o.width):
-      if(img[i][img.width-j][3]==0):
+      if(img[i,img.width-j][3]==0):
         greatest_from_row=j
     pixels_to_be_cropped.append(greatest_from_row)
   cropped_img=img_o.crop(0,0,min(pixels_to_be_cropped),0)
