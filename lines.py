1f4ec977b0c3d98621643dda0e23d8b48be6993c
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-03-18 16:16:56 +0200

    Update lines.py

--- lines.py
+++ lines.py
@@ -3,7 +3,7 @@ import os
 for S2_product in os.listdir('/home/heido/projects/heido_test/s2_zip'):
   for filename in os.listdir('/home/heido/projects/heido_test/s2_zip/'+S2_product+'/GRANULE/'):
     if("L2" in filename):
-      line="/snap/snap8/bin/gpt Write -Ssource=\'/home/heido/projects/heido_test/s2_zip/'+S2_product+'/GRANULE/'+filename+'/\' -Pfile=\'/home/heido/projects/heido_test/s2_zip/'+S2_product+'/GRANULE/'+filename+'/output.dim"
+      line="/snap/snap8/bin/gpt Write -Ssource=\'/home/heido/projects/heido_test/s2_zip/"+S2_product+"/GRANULE/"+filename+"/\' -Pfile=\'/home/heido/projects/heido_test/s2_zip/"+S2_product+"/GRANULE/"+filename+"/output.dim"
       print(line)
   
 
