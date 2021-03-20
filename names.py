5ca9d415246074746d97ae71d47d292a3b02181a
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-03-20 09:59:59 +0200

    Update names.py

--- names.py
+++ names.py
@@ -5,9 +5,9 @@ S1_zip="/home/heido/projects/heido_test/s1_zip"
 
 def S1_short(S1):
   if("." in S1.split("_")[-1]):
-    return S1.split("_"[4])+"_"+S1.split("_")[-1].split(".")[0]
+    return S1.split("_")[4]+"_"+S1.split("_")[-1].split(".")[0]
   else:
-    return S1.split("_"[4])+"_"+S1.split("_")[-1]
+    return S1.split("_")[4]+"_"+S1.split("_")[-1]
   
 def S2_short(S2):
   return S2.split("_")[5]+"_"+S2.split("_")[2]
