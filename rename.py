029e146e52460a4eefcdc82b6eb57da0009dcd17
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-03-20 11:00:41 +0200

    Update rename.py

--- rename.py
+++ rename.py
@@ -5,7 +5,7 @@ def S2_short(S2):
 
 for S2 in os.listdir("/home/heido/projects/NDVI_data/S2_RGB_tiles"):
   date=S2.split("_")[0]
-  for S2safe in oslistdir("s2_zip"):
+  for S2safe in os.listdir("s2_zip"):
     if(S2safe.split("_")[2].split("T")[0]==date):
       S2name=S2_short(S2_safe)
   newname=S2.replace(date,S2name)
