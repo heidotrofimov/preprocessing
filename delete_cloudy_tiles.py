be1bc712c74eb61222b3d6e26f27aba4012786a6
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-03-22 13:39:24 +0200

    Update delete_cloudy_tiles.py

--- delete_cloudy_tiles.py
+++ delete_cloudy_tiles.py
@@ -6,7 +6,7 @@ f=open("cloudless_tiles.txt","r")
 cloudless_tiles=[]
 
 for line in f.readlines():
-  cloudless_tiles.append(line.rstrip()))
+  cloudless_tiles.append(line.rstrip())
   
 for filename in os.listdir("home/heido/projects/NDVI_data/S2_RGB_tiles"):
   if(filename not in cloudless_tiles):
