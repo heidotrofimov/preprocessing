75914a08b95345fabef1b3818dc041e60e5e1f06
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-03-22 14:20:31 +0200

    Update add_history.py

--- add_history.py
+++ add_history.py
@@ -2,7 +2,7 @@ import os
 from PIL import Image
 from datetime import datetime
 
-for filename in os.listdir("/home/heido/projects/NDVI_data/merged_tiles_RGB/")
+for filename in os.listdir("/home/heido/projects/NDVI_data/merged_tiles_RGB/"):
     S2_date_str=filename.split("_")[1].split("T")[0]
     S2_date=datetime.strptime(S2_date_str, '%Y%m%d').date()
     print(S2_date)
