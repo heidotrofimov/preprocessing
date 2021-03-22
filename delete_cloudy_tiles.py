e5f8e482172b2ae2fadfac650fefc2ffc6d0da05
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-03-22 13:53:14 +0200

    Update delete_cloudy_tiles.py

--- delete_cloudy_tiles.py
+++ delete_cloudy_tiles.py
@@ -18,6 +18,10 @@ for filename in os.listdir("/home/heido/projects/NDVI_data/S2_NDVI_tiles"):
     
 for filename in os.listdir("/home/heido/projects/NDVI_data/merged_tiles_RGB"):
   filename2=filename.split("_")[0]+"_"+filename.split("_")[1]+"_"+filename.split("_")[4]+"_"+filename.split("_")[5]
-  print(filename2)
   if(filename2 not in cloudless_tiles):
     os.remove("/home/heido/projects/NDVI_data/merged_tiles_RGB/"+filename)
+    
+for filename in os.listdir("/home/heido/projects/NDVI_data/merged_tiles_NDVI"):
+  filename2=filename.split("_")[0]+"_"+filename.split("_")[1]+"_"+filename.split("_")[4]+"_"+filename.split("_")[5]
+  if(filename2 not in cloudless_tiles):
+    os.remove("/home/heido/projects/NDVI_data/merged_tiles_NDVI/"+filename)
