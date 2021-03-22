adede196ac2065c9e649c43b52d134719e63dd68
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-03-22 14:58:15 +0200

    Update add_history.py

--- add_history.py
+++ add_history.py
@@ -3,9 +3,9 @@ from PIL import Image
 from datetime import datetime
 
 for filename in os.listdir("/home/heido/projects/NDVI_data/merged_tiles_RGB/"):
+    print(filename)
     S2_date_str=filename.split("_")[1].split("T")[0]
     S2_date=datetime.strptime(S2_date_str, '%Y%m%d').date()
-    
     tile_nr=filename.split("_")[-2]+"_"+filename.split("_")[-1]
     for filename2 in os.listdir("/home/heido/projects/NDVI_data/S2_RGB_tiles/"):
         history_date_str=filename2.split("_")[1].split("T")[0]
@@ -14,10 +14,13 @@ for filename in os.listdir("/home/heido/projects/NDVI_data/merged_tiles_RGB/"):
         history_tile_nr=filename2.split("_")[-2]+"_"+filename2.split("_")[-1]
         if(history_date<S2_date and tile_nr==history_tile_nr):
             newname=filename.split("_")[0]+"_"+filename.split("_")[1]+"_"+filename.split("_")[2]+"_"+filename.split("_")[3]+"_"+filename2.split("_")[1]+"_"+tile_nr
-            target=Image.open("/home/heido/projects/NDVI_data/merged_tiles_RGB/"+filename)
-            history=Image.open("/home/heido/projects/NDVI_data/S2_RGB_tiles/"+filename2)
+            #target=Image.open("/home/heido/projects/NDVI_data/merged_tiles_RGB/"+filename)
+            #history=Image.open("/home/heido/projects/NDVI_data/S2_RGB_tiles/"+filename2)
+            target=Image.open("/home/heido/projects/NDVI_data/merged_tiles_NDVI/"+filename)
+            history=Image.open("/home/heido/projects/NDVI_data/S2_NDVI_tiles/"+filename2)
             dst = Image.new('RGB', (target.width + history.width, target.height))
             dst.paste(target, (0, 0))
             dst.paste(history, (target.width, 0))
-            dst.save("/home/heido/projects/NDVI_data/merged_tiles_RGB_wh/"+newname)
+            #dst.save("/home/heido/projects/NDVI_data/merged_tiles_NDVI_wh/"+newname)
+            dst.save("/home/heido/projects/NDVI_data/merged_tiles_NDVI_wh/"+newname)
             break
