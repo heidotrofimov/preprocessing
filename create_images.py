bda17a0064d8067116a71648f91bfbec0c8c4ef8
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-03-23 15:17:13 +0200

    Update create_images.py

--- create_images.py
+++ create_images.py
@@ -15,6 +15,7 @@ from snappy import GPF
 from snappy import HashMap
 import os
 import shutil
+from datetime import datetime
 
 S1_big_images="/home/heido/projects/NDVI_data/S1"
 S2_RGB_big_images="/home/heido/projects/NDVI_data/S2_RGB"
@@ -28,6 +29,22 @@ def S1_short(S1):
     return S1.split("_")[4]+"_"+S1.split("_")[-1].split(".")[0]
   else:
     return S1.split("_")[4]+"_"+S1.split("_")[-1]
+  
+#Save the full names
+
+S2_zip="s2_zip"
+S1_zip="s1_zip"
+
+f = open("full_names.txt","a")
+
+for S2 in os.listdir(S2_zip):
+  date=S2.split("_")[2].split("T")[0]
+  f.write(S2_short(S2)+": "+S2+"\n")
+  for S1 in os.listdir(S1_zip):
+    if(S1.split("_")[4].split("T")[0]==date):
+      f.write(S1_short(S1)+": "+S1+"\n")
+      
+f.close()
 
 jpy = snappy.jpy
 ImageManager = jpy.get_type('org.esa.snap.core.image.ImageManager')
