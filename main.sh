cf22f0621af7034fd2af98aaaa2bafc60259b9c5
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-03-23 15:21:46 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#Before this file is run: download data to s2_zip, s1_zip
+
 python delete_unnecessary_data.py before
 
 python prepare_for_gpt.py
@@ -14,4 +16,6 @@ python delete_unnecessary_data.py after
 
 python create_images.py
 
+
+#After this file has run: delete cloudy tiles from S2_RGB_tiles, run "delete_cloudy_tiles.py" (will delete corresponding tiles from all other directories) and "add_history.py"
 
