ef3ad24549c004cc2f730c1a82cc9fbfff695863
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-03-23 15:19:13 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,9 +2,16 @@
 
 python delete_unnecessary_data.py before
 
-python lines.py S2
+python prepare_for_gpt.py
+
 while IFS= read -r line; do
 $line
 done < lines_for_gpt.txt
 
 rm lines_for_gpt.txt
+
+python delete_unnecessary_data.py after
+
+python create_images.py
+
+
