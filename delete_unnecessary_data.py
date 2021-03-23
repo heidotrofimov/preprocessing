1a2f5c482a1442d05588819579e86bd03e35d563
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-03-23 14:20:31 +0200

    Update and rename delete_S1.py to delete_unnecessary_data.py

---
+++ delete_unnecessary_data.py
@@ -0,0 +1,32 @@
+import os
+import shutil
+
+S2_dates=[]
+
+for folder in os.listdir('s2_zip'):
+    S2_dates.append(folder.split("_")[2].split("T")[0])
+
+
+for folder in os.listdir('s1_zip'):
+    date=folder.split("_")[4].split("T")[0]
+    if(date not in S2_dates):
+        shutil.rmtree('s1_zip/'+folder)
+        
+for folder in os.listdir('collocated'):
+    if(".data" in folder):
+        for filename in os.listdir('collocated/'+folder):
+            if(os.path.isfile('collocated/'+folder+'/'+filename)):
+                if("Amplitude" not in filename):
+                    os.remove('collocated/'+folder+'/'+filename)
+            else:
+                shutil.rmtree('collocated/'+folder+'/'+filename)
+
+
+for folder in os.listdir("s2_zip"):
+    os.remove('s2_zip/'+folder+'/manifest.safe')
+    for folder2 in os.listdir('s2_zip/'+folder+'/GRANULE/'):
+        for filename in os.listdir('s2_zip/'+folder+'/GRANULE/'+folder2+'/IMG_DATA/R10m'):
+            if("B02" not in filename and "B03" not in filename and "B04" not in filename and "B08" not in filename):
+                os.remove('s2_zip/'+folder+'/GRANULE/'+folder2+'/IMG_DATA/R10m/'+filename)
+        shutil.rmtree('s2_zip/'+folder+'/GRANULE/'+folder2+'/IMG_DATA/R20m')
+        shutil.rmtree('s2_zip/'+folder+'/GRANULE/'+folder2+'/IMG_DATA/R60m')
