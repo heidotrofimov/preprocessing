3e1e4872c32fcc7989920e84d7ba5e32b167c3cd
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-04-07 11:03:45 +0300

    Create rename_tiffs.py

---
+++ rename_tiffs.py
@@ -0,0 +1,22 @@
+import os
+sys.path.append('/home/heido/jpy/build/lib.linux-x86_64-3.6')
+sys.path.append('/home/heido/.snap/snap-python')
+import snappy
+from snappy import ProductIO
+import numpy as np
+import matplotlib.pyplot as plt
+from snappy import ProductData
+from PIL import Image
+from snappy import ProductUtils
+from snappy import ProgressMonitor
+from snappy import Product
+from snappy import FlagCoding
+from snappy import GPF
+from snappy import HashMap
+
+
+for dim in os.listdir('/home/heido/projects/heido_test/s1_iq/'):
+  if('.dim' in dim):
+    product=ProductIO.readProduct('/home/heido/projects/heido_test/s1_iq/'+dim):
+      print(product.getMetadataRoot())
+    
