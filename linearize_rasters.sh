4400ff8451231d34322ae8546498d0847dcdb048
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-04-07 17:13:41 +0300

    Update linearize_rasters.sh

--- linearize_rasters.sh
+++ linearize_rasters.sh
@@ -28,7 +28,7 @@ for raster_file in *.tif; do
     band3=$prefix"_cohvv.tif"     # coherence in VV-polarization
     band4=$prefix"_cohvh.tif"     # coherence in VH-polarization
     band5=$prefix"_linc.tif"      # local incidence angle
-    band6=$prefix"_s0vhvv.tif"    # ratio of band2 & band1
+    #band6=$prefix"_s0vhvv.tif"    # ratio of band2 & band1
 
     gdal_calc.py -A $raster_file --A_band=1 --outfile=$band1 --type=Float32 --NoDataValue=-32768 --calc="10**(A.astype(numpy.float32)/1e4 - 2.0)" &>/dev/null
     gdal_calc.py -A $raster_file --A_band=2 --outfile=$band2 --type=Float32 --NoDataValue=-32768 --calc="10**(A.astype(numpy.float32)/1e4 - 2.0)" &>/dev/null
@@ -38,7 +38,9 @@ for raster_file in *.tif; do
     gdal_calc.py -A $band1 -B $band2 --outfile=$band6 --type=Float32 --NoDataValue=-32768 --quiet --calc="B/A"
 
     # virtual raster to bind all the separate bands into a single file
-    gdalbuildvrt -q -separate $prefix.vrt $band1 $band2 $band3 $band4 $band5 $band6
+    gdalbuildvrt -q -separate $prefix.vrt $band1 $band2 $band3 $band4 $band5
+    rm $prefix.tif
+    gdal_translate $prefix.vrt $prefix.tif
 done
 
 mv $outdir/* .
