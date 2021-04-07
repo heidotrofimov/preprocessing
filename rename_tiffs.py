1fdda6e3da74d5d4ab8fceb160802b03d9bc1dbf
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-04-07 11:34:46 +0300

    Update rename_tiffs.py

--- rename_tiffs.py
+++ rename_tiffs.py
@@ -20,6 +20,6 @@ for dim in os.listdir('/home/heido/projects/heido_test/s1_iq/'):
   if('.dim' in dim):
     identity=dim.split('.')[0]
     product=ProductIO.readProduct('/home/heido/projects/heido_test/s1_iq/'+dim)
-    name=product.getMetadataRoot().getElement('Abstracted_Metadata').getAttribute('PRODUCT').getData()
+    name=str(product.getMetadataRoot().getElement('Abstracted_Metadata').getAttribute('PRODUCT').getData())
     os.rename('/home/heido/projects/heido_test/s1_iq/'+identity+'.tif','/home/heido/projects/heido_test/s1_iq/'+name+'.tif')
     
