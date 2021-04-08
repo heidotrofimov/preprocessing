2ebcff8998a3617da45e034080bb79f639ff51d4
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-04-08 18:04:16 +0300

    Update read_tiff.py

--- read_tiff.py
+++ read_tiff.py
@@ -1,7 +1,7 @@
 import cv2
 
-image = cv2.imread('s1_iq/combined.tif', cv2.IMREAD_UNCHANGED)
-#print(image.shape)
+image = cv2.imread('s1_iq/combined.tiff', cv2.IMREAD_UNCHANGED)
+print(image.shape)
 
 channels = cv2.split(image)
 
