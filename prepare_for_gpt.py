d33687bdf9ed4bc7f819d87cc628f100884ff971
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-04-08 20:05:10 +0300

    Update prepare_for_gpt.py

--- prepare_for_gpt.py
+++ prepare_for_gpt.py
@@ -24,9 +24,10 @@ for folder in os.listdir('s1_tif_final'):
         if(days_between_tmp<days_between):
             days_between=days_between_tmp
             chosen=folder2
-   if(chosen_S2!=""):
-    S1.append(folder)
-    S2.append(chosen_S2)
+    if(chosen_S2!=""):
+        
+        S1.append(folder)
+        S2.append(chosen_S2)
 print(S1)
 print(S2)
 
