931e6dca434f932835e1452df2e014a1e6ff12ac
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-04-08 18:33:52 +0300

    Update rename_tiffs.py

--- rename_tiffs.py
+++ rename_tiffs.py
@@ -16,7 +16,7 @@ from snappy import GPF
 from snappy import HashMap
 from shutil import copyfile
 
-
+'''
 for dim in os.listdir('s1_iq/'):
   if('.dim' in dim):
     identity=dim.split('.')[0]
@@ -24,6 +24,10 @@ for dim in os.listdir('s1_iq/'):
     name=str(product.getMetadataRoot().getElement('Abstracted_Metadata').getAttribute('PRODUCT').getData())
     #os.rename('/home/heido/projects/heido_test/s1_iq/'+identity+'.tif','/home/heido/projects/heido_test/s1_iq/'+name+'.tif')
     copyfile('s1_iq/'+identity+'.tif','s1_tif/'+name+'.tif')
+'''
 
+for tif in os.listdir('s1_iq'):
+  if('.tif' in tif and 'cohv' not in tif and 'linc' not in tif and 's0v' not in tif):
+    copyfile('s1_iq/'+tif,'s1_tif/'+tif)
     
     
