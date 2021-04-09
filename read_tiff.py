a3e43725b6f9ddec31319bbda62d1243905eb8de
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-04-09 11:39:52 +0300

    Update read_tiff.py

--- read_tiff.py
+++ read_tiff.py
@@ -1,4 +1,6 @@
 from PIL import Image
 import numpy as np
-img = np.asarray(Image.open('/home/heido/ifg_44_13_9.tif'))
-print(img.dtype)
+from skimage import io
+img = io.imread('/home/heido/ifg_44_13_9.tif')
+#img = np.asarray(Image.open('/home/heido/ifg_44_13_9.tif'))
+#print(img.dtype)
