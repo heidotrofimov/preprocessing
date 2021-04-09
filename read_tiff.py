87389db928159d6ed7a2a67753c86a140e39367d
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-04-09 11:22:46 +0300

    Update read_tiff.py

--- read_tiff.py
+++ read_tiff.py
@@ -1,4 +1,4 @@
 from PIL import Image
 import numpy as np
-img = np.asarray(Image.open('1SDV_8365.tif'))
+img = np.asarray(Image.open('S1B_IW_SLC__1SDV_20200525T042524_20200525T042552_021733_0293FD_C565.tif'))
 print(img.dtype)
