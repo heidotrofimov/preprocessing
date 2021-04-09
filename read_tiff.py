10f46b4cc67ab4fc9e052432cdae73b5cdb6be09
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-04-09 12:00:42 +0300

    Update read_tiff.py

--- read_tiff.py
+++ read_tiff.py
@@ -1,7 +1,11 @@
 from PIL import Image
 import numpy as np
 from skimage import io
-img = io.imread('/home/heido/ifg_44_13_9.tif')
-img = np.asarray(img)
-print(img.dtype)
-print(img.shape)
+
+for tif_file in os.listdir('s1_iq'):
+  if('.tif' in tif_file):
+    print(tif_file)
+    img = io.imread('s1_iq/'+tif_file)
+    img = np.asarray(img)
+    print(img.dtype)
+    print(img.shape)
