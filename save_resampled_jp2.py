817f144c1307fde64ab9ae831191e9d1ab0e12c0
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-04-27 13:16:37 +0300

    Create save_resampled_jp2.py

---
+++ save_resampled_jp2.py
@@ -0,0 +1,36 @@
+import sys
+sys.path.append('/home/heido/jpy/build/lib.linux-x86_64-3.6')
+sys.path.append('/home/heido/.snap/snap-python')
+import snappy
+from snappy import ProductIO
+import numpy as np
+import matplotlib.pyplot as plt
+from snappy import ProductData
+from PIL import Image
+from snappy import ProductUtils
+from snappy import ProgressMonitor
+from snappy import Product
+from snappy import FlagCoding
+from snappy import GPF
+from snappy import HashMap
+import os
+import shutil
+from datetime import datetime
+import sys
+
+jpy = snappy.jpy
+ImageManager = jpy.get_type('org.esa.snap.core.image.ImageManager')
+JAI = jpy.get_type('javax.media.jai.JAI')
+
+def write_rgb_image(bands, filename, format):
+    image_info = ProductUtils.createImageInfo(bands, True, ProgressMonitor.NULL)
+    im = ImageManager.getInstance().createColoredBandImage(bands, image_info, 0)
+    JAI.create("filestore", im, filename, format)
+    
+def write_image(band, filename, format):
+    im = ImageManager.getInstance().createColoredBandImage([band], band.getImageInfo(), 0)
+    JAI.create("filestore", im, filename, format)
+
+product=ProductIO.readProduct(sys.argv[1])
+band_1 = product.getBand('B4')
+write_image(band_1,'B4.jp2', 'JPEG2000')
