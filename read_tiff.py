896fdf7769bc4db3d25747c638d9d5cc08fd9153
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-06-02 14:45:27 +0300

    Update read_tiff.py

--- read_tiff.py
+++ read_tiff.py
@@ -3,4 +3,11 @@ import numpy as np
 from skimage import io
 import os
 
-S1_im=Image.open('collocated_S1.tif')
+i=0
+
+for filename in os.listdir('collocated_tifs/'):
+  if(i==0):
+    S1_im=Image.open('collocated_tifs/'+filename)
+    imarray=np.array(im)
+    print(imarray)
+  i=i+1
