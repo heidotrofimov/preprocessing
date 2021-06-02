c3faf80fd1820c40f21cd0d3a0f9ab99136be615
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-06-02 14:34:26 +0300

    Update save_tif.py

--- save_tif.py
+++ save_tif.py
@@ -3,5 +3,5 @@ import os
 for filename in os.listdir('collocated_2'):
   if(".dim" in filename):
     inputfile='collocated_2/'+filename
-    output='collocated_tifs/'+filename+'.tif'
+    output='collocated_tifs/'+filename.split(".")[0]+'.tif'
     print("/snap/snap8/bin/gpt save_tif.xml -Pinput=\""+inputfile+"\" -Poutput=\""+output+"\"")
