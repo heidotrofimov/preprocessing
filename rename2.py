cd9ec64b1d163d8ee84f5c09422443075d0805b4
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-06-03 19:51:00 +0300

    Update rename2.py

--- rename2.py
+++ rename2.py
@@ -2,6 +2,6 @@ import os
 from shutil import copyfile
 
 for filename in os.listdir("s1_tiles"):
-  new_name=filnemae.replace(".tif_","_")
+  new_name=filename.replace(".tif_","_")
   copyfile("s1_tiles/"+filename,"s1_tiles/"+new_name)
   os.remove("s1_tiles/"+filename)
