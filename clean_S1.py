fb5331dd64ff7e929c9a796caeeadd6917a4b12a
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-06-04 15:30:44 +0300

    Update clean_S1.py

--- clean_S1.py
+++ clean_S1.py
@@ -3,6 +3,7 @@ from shutil import copyfile
 
 for filename in os.listdir('s2_RGB_cloudfree'):
   for filename2 in os.listdir("s1_final_tiles"):
-    if(filename.split(".")[0]==filename.split(".")[0]):
+    if(filename.split(".")[0]==filename2.split(".")[0]):
       copyfile("s1_final_tiles/"+filename2,"s1_final_tiles_cloudfree/"+filename2)
+      break
       
