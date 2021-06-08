120193dd875a6ae1671daa3e7212575e5702711c
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-06-08 11:11:01 +0300

    Update prepare_for_gpt.py

--- prepare_for_gpt.py
+++ prepare_for_gpt.py
@@ -9,8 +9,36 @@ B02_list=[]
 B02name_list=[]
 targetpath_list=[]
 
-for folder in os.listdir('s1_tif_final_2'):
-    date_str=folder.split("_")[5].split("T")[0]
+def S1_name(S1_full):
+    S1=S1_full.split(".")[0].split("_")
+    return S1[0]+"_"+S1[4]+"_"+S1[-3]+"_"+S1[-2]+"_"+S1[-1]
+
+def S2_name(S2_full):
+    S2=S2_full.split(".")[0].split("_")
+    return S2[0]+"_"+S2[2]+"_"+S2[5]
+
+for S1_tif in os.listdir('s1_tif_final'):
+    S1p='s1_tif_final/'+S1_tif
+    date1=S1_tif.split("_")[5].split("T")[0]
+    for S2_product in os.listdir("s2_zip"):
+        date2=S2_product.split("_")[2].split("T")[0]
+        if(date1==date2):
+            
+            name=S1_name(S1_tif)+"_colwith_"+S2_name(S2_product)
+            for folder3 in os.listdir('s2_zip/'+S2_product+'/GRANULE/'):
+            if("L2" in folder3):
+                for filename in os.listdir('s2_zip/'+S2_product+'/GRANULE/'+folder3+'/IMG_DATA/R10m/'):
+                    if('B02_10m.jp2' in filename):
+                        B02='/home/heido/projects/preprocessing/s2_zip/'+S2_product+'/GRANULE/'+folder3+'/IMG_DATA/R10m/'+filename
+                        B02name=filename
+            targetpath='/home/heido/projects/preprocessing/collocated/'+name+".dim"
+            f.write("/snap/snap8/bin/gpt collocation.xml -PB02=\""+B02+"\" -PS1=\""+S1p+"\" -PB02name=\""+B02name+"\" -Ptargetpath=\""+targetpath+"\"\n")
+            
+                        
+            
+        
+    
+    '''
     year=int(date_str[0:4])
     month=int(date_str[4:6])
     day=int(date_str[6:8])
@@ -46,13 +74,13 @@ for folder in os.listdir('s1_tif_final_2'):
         B02_list.append(B02)
         B02name_list.append(B02name)
         targetpath_list.append(targetpath)
-        
+     '''   
 
-        
+'''        
 for i in range(len(S1)):
     f.write("/snap/snap8/bin/gpt collocation.xml -PB02=\""+B02_list[i]+"\" -PS1=\""+S1[i]+"\" -PB02name=\""+B02name_list[i]+"\" -Ptargetpath=\""+targetpath_list[i]+"\"\n")
 f.close()
-'''
+
 for folder in os.listdir('s1_zip'):
     date=folder.split("_")[4].split("T")[0]
     for folder2 in os.listdir('s2_zip'):
