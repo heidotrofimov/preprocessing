3bf36d8c7ad37ee5d0207fd03e1db8b59b1de9ef
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-06-08 11:36:46 +0300

    Update combine_tiffs.sh

--- combine_tiffs.sh
+++ combine_tiffs.sh
@@ -3,5 +3,5 @@
 for raster_file in $1/*.vrt; do
     echo $raster_file
     filename=$(basename $raster_file .vrt)
-    gdal_translate $raster_file s1_tif_final_2/$filename.tif
+    gdal_translate $raster_file s1_tif_final/$filename.tif
    done
