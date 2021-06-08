6e6633495160fbf4a881302b5d1735453b254dcc
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-06-08 12:49:45 +0300

    Create s2_processor.py

---
+++ s2_processor.py
@@ -0,0 +1,68 @@
+import sys
+sys.path.append('/home/heido/jpy/build/lib.linux-x86_64-3.6')
+sys.path.append('/home/heido/.snap/snap-python')
+import snappy
+from snappy import ProductIO
+import numpy as np
+import matplotlib.pyplot as plt
+from snappy import ProductData
+from PIL import Image
+from snappy import ProductUtils
+from snappy import ProgressMonitor
+from snappy import Product
+from snappy import FlagCoding
+from snappy import GPF
+from snappy import HashMap
+import os
+import shutil
+from datetime import datetime
+
+jpy = snappy.jpy
+ImageManager = jpy.get_type('org.esa.snap.core.image.ImageManager')
+JAI = jpy.get_type('javax.media.jai.JAI')
+
+#Biomass environment for this script!
+
+def write_rgb_image(bands, filename, format):
+    image_info = ProductUtils.createImageInfo(bands, True, ProgressMonitor.NULL)
+    im = ImageManager.getInstance().createColoredBandImage(bands, image_info, 0)
+    JAI.create("filestore", im, filename, format)
+    
+def write_image(band, filename, format):
+    im = ImageManager.getInstance().createColoredBandImage([band], band.getImageInfo(), 0)
+    JAI.create("filestore", im, filename, format)
+    
+def S2_short(S2_full):
+    S2=S2_full.split(".")[0].split("_")
+    return S2[0]+"_"+S2[2]+"_"+S2[5]
+  
+  
+for S2_SAFE in os.listdir('s2_zip'):
+    RGB_im=S2_SAFE.split(".")[0]
+    if(os.path.isfile("S2_images/"+RGB_im+".png")==False):
+      S2_product=ProductIO.readProduct('s2_zip/'+S2_SAFE+'/GRANULE/output.dim')
+      band_names = S2_product.getBandNames()
+      red = S2_product.getBand('B4')
+      green = S2_product.getBand('B3')
+      blue = S2_product.getBand('B2')
+      write_rgb_image([red, green, blue], RGB_im, 'png')
+      shutil.move(RGB_im,'S2_images')
+    
+
+for RGB_im in os.listdir("S2_images"):
+  name=RGB_im.split(".")[0]
+  im_S2 = Image.open("S2_images/"+RGB_im)
+  tiles_x=int(im_S2.width/tile_size)
+  tiles_y=int(im_S2.height/tile_size)
+  for i in range(0,tiles_x):
+    for j in range(0,tiles_y):
+      tile="_"+str(i)+"_"+str(j)
+      #Is this tile cloudfree?
+      for tile2 in os.listdir("home/heido/projects/cm_predict/prediction/"+name):
+        tile3=tile2.split("ile")[1]
+        if(tile=tile3):
+          clear=Image.open("home/heido/projects/cm_predict/prediction/"+name+"/"+tile2+"/predict_CLEAR.png")
+          cm=np.array(clear,dtype=np.float)
+          print(m)
+              
+  
