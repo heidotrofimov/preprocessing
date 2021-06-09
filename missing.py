67992e56032b20eedc3cd51ae80a1f571e95f68b
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-06-09 16:55:40 +0300

    Update missing.py

--- missing.py
+++ missing.py
@@ -6,16 +6,22 @@ f2=open("missing.txt","w")
 f= open("s2_data/products.dat","r")
 lines = f.readlines()
 
+missing=[]
+
 for line in lines:
   olemas=False
   for filename in os.listdir("s2_data"):
     if(".SAFE" in filename):
+      print(filename)
       if(line==filename.split(".SAFE")[0]):
         olemas=True
   if(olemas==False):
     f2.write(line)
+    missing.append(line)
     
 f.close()
 f2.close()
+
+print(len(missing))
     
   
