2ea8d2c4e78e3868610f66433dd4c94a20d9e22e
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-06-09 14:30:30 +0300

    Update merge.py

--- merge.py
+++ merge.py
@@ -3,14 +3,15 @@ from shutil import copyfile
 
 copied=[]
 
-for filename in os.listdir("s2_NDVI/"):
-  s2name=filename.split(".")[0]
-  for filename2 in os.listdir("s1_tiles"):
-    name=filename2.split("colwith_")[1].split(".")[0]
-    if(name==s2name):
-      if(filename not in copied):
-        copyfile("s2_NDVI/"+filename,"data/S2/"+filename)
-        copied.append(filename)
-      copyfile("s1_tiles/"+filename2,"data/S1/"+filename2)
+for filename in os.listdir("s1_tiles/"):
+  corresponding_s2=filename.split("colwith_")[1].split(".")[0]
+  for filename2 in os.listdir("s2_NDVI"):
+    s2name=filename2.split(".")[0]
+    if(s2name==corresponding_s2):
+      copyfile("s1_tiles/"+filename,"data/S1/"+filename)
+      if(filename2 not in copied):
+        copyfile("s2_NDVI/"+filename2,"data/S2/"+filename2)
+        copied.append(filename2)
+      
       
     
