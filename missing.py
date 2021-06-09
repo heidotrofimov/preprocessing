ecc4669401f229fe5ae86767aa49c22fb68541f0
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-06-09 16:49:37 +0300

    Create missing.py

---
+++ missing.py
@@ -0,0 +1,19 @@
+
+f2=open("missing.txt","r")
+
+f= open("s2_data/products.dat")
+lines = f.readlines()
+
+for line in lines:
+  olemas=False
+  for filename in os.listdir("s2_data"):
+    if(".SAFE" in filename):
+      if(line==filename.split(".SAFE")[0]):
+        olemas=True
+  if(olemas==False):
+    missing.write(line+"\n")
+    
+f.close()
+f2.close()
+    
+  
