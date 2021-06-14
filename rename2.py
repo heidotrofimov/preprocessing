094a082923c78176953d3740cd7ecb99b25c77a7
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-06-14 18:36:13 +0300

    Update rename2.py

--- rename2.py
+++ rename2.py
@@ -10,7 +10,7 @@ def S2_name(S2_full):
     return S2[0]+"_"+S2[2]+"_"+S2[5]
 
 s1_dict={}
-'''
+
 f1=open("full_product_names","r")
 
 lines=f1.readlines()
@@ -22,17 +22,26 @@ for line in lines:
   
 f1.close()
 
-for filename in os.listdir("/home/users/biomass/s1_files"):
+for filename in os.listdir("/home/users/biomass/s2_RGB"):
+  '''
   s1_part=filename.split("_")[2]+"_"+filename.split("_")[3]
   s1_long=s1_dict[s1_part]
   s1_real=S1_name(s1_long)
+  '''
   s2_part=filename.split("_")[0]+"_"+filename.split("_")[1]
   s2_long=s1_dict[s2_part]
-  s2_real=S2_name(s2_long)
-  
+  s2_real=S2_name(s2_long).replace(" ","")
+  s2_real=s2_real.replace("'","")
+  new_name=s2_real+"_"+filename.split("_")[4]+"_"+filename.split("_")[5]
+  print(new_name)
+
+  '''
   new_name=s1_real+"_colwith_"+s2_real+"_"+filename.split("_")[4]+"_"+filename.split("_")[5]+".tif"
   copyfile("/home/users/biomass/s1_files/"+filename,"/home/users/biomass/s1_files/"+new_name)
   os.remove("/home/users/biomass/s1_files/"+filename)
+  '''
+    
+    
 '''
 
 for filename in os.listdir("/home/users/biomass/s1_files"):
@@ -43,3 +52,4 @@ for filename in os.listdir("/home/users/biomass/s1_files"):
     copyfile("/home/users/biomass/s1_files/"+filename,"/home/users/biomass/s1_files/"+newname)
     os.remove("/home/users/biomass/s1_files/"+filename)
     
+'''
