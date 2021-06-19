c4316e5a161c8f7aa9a3279beb92b6f544672f04
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-06-19 11:37:27 +0300

    Update add_history.py

--- add_history.py
+++ add_history.py
@@ -1,6 +1,7 @@
 import os
 from PIL import Image
 from datetime import datetime, timedelta
+from shutil import copyfile
 
 for filename in os.listdir("data/S2/"):
     AOI1=filename.split("_")[2]
@@ -14,4 +15,14 @@ for filename in os.listdir("data/S2/"):
         tile_nr2=filename2.split(AOI2+"_")[1].split(".")[0]
         if(AOI1==AOI2 and tile_nr1==tile_nr2 and date_time_obj2<date_time_obj1 and (date_time_obj1-date_time_obj2).days<32):
             print(filename+" with "+filename2)
+            S2_filename=filename.split("AOI1")[0]+AOI1+"_"+filename2.split("_")[0]+"_"+filename2.split("_")[1]+"_"+tile_nr1+".png"
+            print("New S2 filename: "+S2_filename)
+            print(S2_filename)
+            for filename3 in os.listdir("data/S1"):
+                corresponding_S2=filename3.split("colwith_")[1].replace(".tif",".png")
+                if(filename3==filename):
+                    S1_filename=filename3.split("colwith_")[0]+S2_filename.split(".")[0]+".tif"
+            print("New S1 filename: "+S1_filename)
+            
+            
         
