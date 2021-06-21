7e9127b100f617f90510c8958f44fd8b71418786
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-06-21 13:30:04 +0300

    Update delete_unnecessary_data.py

--- delete_unnecessary_data.py
+++ delete_unnecessary_data.py
@@ -8,10 +8,6 @@ for tif in os.listdir("s1_tif_final"):
     S1_short=tif.split("_")[0]+"_"+tif.split("_")[5]+"_"+tif.split("_")[-1].split(".")[0]
     for collocated in os.listdir("collocated"):
         if(collocated.split("_colwith")[0]==S1_short):
-            print(tif)
-            print(S1_short)
-            deleted+=1
-            break
+            os.remove("s1_tif_final/"+tif)
             
-print(deleted)
 
