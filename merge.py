82f853d74eb5950f7473e6948a242ff765acd3db
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-08-08 19:03:20 +0300

    Update merge.py

--- merge.py
+++ merge.py
@@ -4,9 +4,6 @@ from shutil import copyfile
 from PIL import Image
 from datetime import datetime, timedelta
 
-year="2020"
-AOI="T35VMC"
-
 #Without history:
 
 for s2 in os.listdir("s2_NDVI"):
@@ -38,21 +35,18 @@ for s2 in os.listdir("s2_NDVI"):
 for filename in os.listdir("data/S2/"):
     AOI1=filename.split("_")[2]
     date_str1=filename.split("_")[1].split("T")[0]
-    year1=date_str1[0:4]
-    if(AOI1==AOI and year1==year):
-      
-      date_time_obj1 = datetime(int(date_str1[0:4]),int(date_str1[4:6]),int(date_str1[6:8]))
-      tile_nr1=filename.split(AOI1+"_")[1].split(".")[0]
-      list_of_suitables=[]
-      max_days=32
-      for filename2 in os.listdir("s2_NDVI/"):
-          AOI2=filename2.split("_")[2]
-          date_str2=filename2.split("_")[1].split("T")[0]
-          date_time_obj2 = datetime(int(date_str2[0:4]),int(date_str2[4:6]),int(date_str2[6:8]))
-          tile_nr2=filename2.split(AOI2+"_")[1].split(".")[0]
-          if(AOI1==AOI2 and tile_nr1==tile_nr2 and date_time_obj2<date_time_obj1 and (date_time_obj1-date_time_obj2).days<max_days):
-              found_historical=filename2
-              max_days=(date_time_obj1-date_time_obj2).days
+    date_time_obj1 = datetime(int(date_str1[0:4]),int(date_str1[4:6]),int(date_str1[6:8]))
+    tile_nr1=filename.split(AOI1+"_")[1].split(".")[0]
+    list_of_suitables=[]
+    max_days=32
+    for filename2 in os.listdir("s2_NDVI/"):
+        AOI2=filename2.split("_")[2]
+        date_str2=filename2.split("_")[1].split("T")[0]
+        date_time_obj2 = datetime(int(date_str2[0:4]),int(date_str2[4:6]),int(date_str2[6:8]))
+        tile_nr2=filename2.split(AOI2+"_")[1].split(".")[0]
+        if(AOI1==AOI2 and tile_nr1==tile_nr2 and date_time_obj2<date_time_obj1 and (date_time_obj1-date_time_obj2).days<max_days):
+            found_historical=filename2
+            max_days=(date_time_obj1-date_time_obj2).days
     if(max_days<32):
         print(filename+" with "+found_historical)
         S2_filename=filename.split(AOI1)[0]+AOI1+"_"+found_historical.split("_")[0]+"_"+found_historical.split("_")[1]+"_"+tile_nr1+".png"
