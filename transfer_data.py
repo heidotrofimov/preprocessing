a69433090a89fba42f9f6f8dc938d7d24aa209f9
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-08-10 10:39:12 +0300

    Create transfer_data.py

---
+++ transfer_data.py
@@ -0,0 +1,17 @@
+import os
+
+destination="/home/users/biomass/"
+home="data/"
+
+folders=["S1/","S2/","S2_RGB/","with_history/S1","with_history/S2","with_history/S2_RGB/"]
+for folder in folders:
+  for filename in os.listdir(home+folder):
+    moved=False
+    for filename2 in os.listdir(destintion+folder):
+      if(filename==filename2):
+        moved=True
+    if(moved==False):
+      os.system("cp "+home+folder+filename+" "+destination+folder+filename)
+    
+    
+
