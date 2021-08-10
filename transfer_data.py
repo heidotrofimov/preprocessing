ab81fd6631c5e93d8453daecc510f848be250918
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-08-10 10:39:49 +0300

    Update transfer_data.py

--- transfer_data.py
+++ transfer_data.py
@@ -7,7 +7,7 @@ folders=["S1/","S2/","S2_RGB/","with_history/S1","with_history/S2","with_history
 for folder in folders:
   for filename in os.listdir(home+folder):
     moved=False
-    for filename2 in os.listdir(destintion+folder):
+    for filename2 in os.listdir(destination+folder):
       if(filename==filename2):
         moved=True
     if(moved==False):
