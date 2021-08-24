2ba79b514d515eb64cc0789ffa27f473661e9a4c
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-08-24 10:04:03 +0300

    Delete save_tif.py

--- save_tif.py
+++
@@ -1,7 +0,0 @@
-import os
-
-for filename in os.listdir('collocated'):
-  if(".dim" in filename):
-    inputfile='collocated/'+filename
-    output='collocated_tifs/'+filename.split(".")[0]+'.tif'
-    print("/snap/snap8/bin/gpt save_tif.xml -Pinput=\""+inputfile+"\" -Poutput=\""+output+"\"")
