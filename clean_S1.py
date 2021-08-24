8c6d37f7a68fc7ad617d26144c6686ff320dd2fd
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-08-24 09:58:24 +0300

    Delete clean_S1.py

--- clean_S1.py
+++
@@ -1,9 +0,0 @@
-import os
-from shutil import copyfile
-
-for filename in os.listdir('s2_RGB_cloudfree'):
-  for filename2 in os.listdir("s1_final_tiles"):
-    if(filename.split(".")[0]==filename2.split(".")[0]):
-      copyfile("s1_final_tiles/"+filename2,"s1_final_tiles_cloudfree/"+filename2)
-      break
-      
