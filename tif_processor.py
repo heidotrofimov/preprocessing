c75e309662781478952c784db141c89eefee9011
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-09-03 17:45:22 +0300

    Update tif_processor.py

--- tif_processor.py
+++ tif_processor.py
@@ -43,10 +43,9 @@ f=open("../find_products/list_for_senpy.txt","r")
 nr=f.readlines()
 f.close()
 
-for n in range(1):
-    '''
+for n in range(3):
     while(os.path.isfile("senpy_ready.txt")==False):
-        time.sleep(900)
+        time.sleep(300)
         print("Waiting for senpy")
     os.system("rm senpy_ready.txt")
     print("Senpy ready, starting to process!")
@@ -56,7 +55,7 @@ for n in range(1):
     os.system("bash linearize_rasters.sh s1_tif")
     os.system("bash combine_tiffs.sh s1_tif")
     os.system("rm s1_tif/*")
-    '''
+    
     for S1_tif in os.listdir('s1_tif_final'):
         S1p='s1_tif_final/'+S1_tif
         date1=S1_tif.split("_")[5].split("T")[0]
