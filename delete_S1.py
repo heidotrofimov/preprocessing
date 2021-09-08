d6650e0e8a8ace80a62bd4da00c34c64947b7fed
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-09-08 12:31:01 +0300

    Update delete_S1.py

--- delete_S1.py
+++ delete_S1.py
@@ -1,27 +1,6 @@
 import os
 from datetime import datetime, timedelta
 
-for S1 in os.listdir("data/S1"):
-  S1p=S1.split("_colwith")[0]
-  parts=S1p.split("_")
-  if(len(parts)>3):
-    print(S1)
-    date=S1.split("_")[7]
-    newname=parts[0]+"_"+date+"_"+parts[4]+"_colwith_"+S1.split("colwith_")[1]
-    print(newname)
-    os.system("mv data/S1/"+S1+" data/S1/"+newname)
-    
-for S1 in os.listdir("data/with_history/S1"):
-  S1p=S1.split("_colwith")[0]
-  parts=S1p.split("_")
-  if(len(parts)>3):
-    print(S1)
-    date=S1.split("_")[7]
-    newname=parts[0]+"_"+date+"_"+parts[4]+"_colwith_"+S1.split("colwith_")[1]
-    print(newname)
-    os.system("mv data/with_history/S1/"+S1+" data/with_history/S1/"+newname)
-
-'''
 for S2 in os.listdir("data/S2"):
   S2=S2.split(".")[0]
   date_S2_str=S2.split("_")[1].split("T")[0]
@@ -50,7 +29,6 @@ for S2 in os.listdir("data/S2"):
         to_be_deleted.append(S1)
     print("To be deleted:")
     print(to_be_deleted)
-'''      
 
 
 
