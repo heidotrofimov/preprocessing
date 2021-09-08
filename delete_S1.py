d6001ff21963e621b9e49066ba553f9fc02e3bf0
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-09-08 12:31:21 +0300

    Update delete_S1.py

--- delete_S1.py
+++ delete_S1.py
@@ -19,7 +19,7 @@ for S2 in os.listdir("data/S2"):
       date_str=S1.split("_")[1].split("T")[0]
       date_S1=datetime(int(date_str[0:4]),int(date_str[4:6]),int(date_str[6:8]))
       days_between=date_S2-date_S1
-      if(days_between<mind_days):
+      if(days_between<min_days):
         min_days=days_between
     for S1 in S1s:
       date_str=S1.split("_")[1].split("T")[0]
