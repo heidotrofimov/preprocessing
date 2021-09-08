0a501a0859bddb2a0524b0ddd07b48a7f5f58148
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-09-08 13:05:41 +0300

    Update delete_S1.py

--- delete_S1.py
+++ delete_S1.py
@@ -2,18 +2,18 @@ import os
 from datetime import datetime, timedelta
 import numpy as np
 
-for S2 in os.listdir("data/S2"):
+for S2 in os.listdir("data/with_history/S2"):
   S2=S2.split(".")[0]
   date_S2_str=S2.split("_")[1].split("T")[0]
   date_S2=datetime(int(date_S2_str[0:4]),int(date_S2_str[4:6]),int(date_S2_str[6:8]))
   S1s=[]
-  for S1 in os.listdir("data/S1"):
+  for S1 in os.listdir("data/with_history/S1"):
     corresponding_S2=S1.split("colwith_")[1].split(".")[0]
     if(corresponding_S2==S2):
       S1s.append(S1)
   if(len(S1s)>1):
-    #print(S2)
-    #print(S1s)
+    print(S2)
+    print(S1s)
     to_be_deleted=[]
     min_days=3
     for S1 in S1s:
@@ -28,8 +28,11 @@ for S2 in os.listdir("data/S2"):
       days_between=np.abs((date_S2-date_S1).days)
       if(days_between!=min_days):
         to_be_deleted.append(S1)
+    print(to_be_deleted)
+    '''
     for d in to_be_deleted:
-      os.system("rm data/S1/"+d)
+      os.system("rm data/with_history/S1/"+d)
+    '''
 
 
 
