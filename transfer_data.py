121fb7154120c0d20b2d39152582f49ce6a5a272
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-09-09 11:50:01 +0300

    Update transfer_data.py

--- transfer_data.py
+++ transfer_data.py
@@ -12,6 +12,20 @@ for folder in folders:
         moved=True
     if(moved==False):
       os.system("cp "+home+folder+filename+" "+destination+folder+filename)
+      
+      
+destination="/home/users/biomass_test_data/"
+home="test_data/"
+
+folders=["S1/","S2/","S2_RGB/","with_history/S1/","with_history/S2/","with_history/S2_RGB/"]
+for folder in folders:
+  for filename in os.listdir(home+folder):
+    moved=False
+    for filename2 in os.listdir(destination+folder):
+      if(filename==filename2):
+        moved=True
+    if(moved==False):
+      os.system("cp "+home+folder+filename+" "+destination+folder+filename)
     
     
 
