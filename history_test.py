387549b029c15bb09212a8c59891af56f7dfff5b
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-09-14 14:13:28 +0300

    Create history_test.py

---
+++ history_test.py
@@ -0,0 +1,27 @@
+import os
+from datetime import datetime, timedelta
+
+history=[]
+
+for S2 in os.listdir("data/S2"):
+  histories=[]
+  histories.append(S2)
+  AOI=S2.split("_")[2]
+  tile=S2.split(AOI+"_")[0]
+  date_str=S2.split("_")[1].split("T")[0]
+  date_obj=datetime(int(date_str[0:4]),int(date_str[4:6]),int(date_str[6:8]))
+  for S2_2 in os.listdir("data/S2"):
+      AOI2=S2.split("_")[2]
+      tile2=S2.split(AOI+"_")[0]
+      date_str2=S2.split("_")[1].split("T")[0]
+      date_obj2=datetime(int(date_str[0:4]),int(date_str[4:6]),int(date_str[6:8]))
+      if(AOI2==AOI and tile2==tile and date_obj2<date_obj):
+        histories.append(S2_2)
+  history.append(histories)
+  
+history2=history.sort(key=len)
+
+for i in range(10):
+  print(history2[-(i+1)])
+    
+  
