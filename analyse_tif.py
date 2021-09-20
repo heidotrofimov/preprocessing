3ca330b2d9df858b1a10281ec58f638eafc52718
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-09-20 19:38:06 +0300

    Update analyse_tif.py

--- analyse_tif.py
+++ analyse_tif.py
@@ -8,6 +8,24 @@ from datetime import datetime, timedelta
 import numpy as np
 import time
 
+
+for filename in os.listdir("s1_tiles"):
+  print(filename)
+  path="s1_tiles/"+filename
+  S1_im=TIFF.open(path)
+  imarray=S1_im.read_image()
+  condition=True
+  for j in range(len(imarray)):
+    for i in range(len(imarray[j])):
+      if(imarray[j][i][0]==0 or (imarray[j][i][1]==-32768 and imarray[j][i][2]==-32768 and imarray[j][i][3]==-32768 and imarray[j][i][4]==-32768)):
+        condition=False
+        os.remove(path)
+        break
+    if(condition==False):
+      break
+
+
+'''
 for filename in os.listdir("s1_tiles"):
   print(filename)
   path="s1_tiles/"+filename
@@ -32,3 +50,4 @@ for filename in os.listdir("s1_tiles"):
       break
   if(condition==True):
     print("Not to be deleted!")
+'''
