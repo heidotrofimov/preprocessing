7d6b39b96ead8aeaffb02d02fa41ac9b33358eb5
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-09-20 17:58:51 +0300

    Create analyse_tif.py

---
+++ analyse_tif.py
@@ -0,0 +1,31 @@
+import os
+import os.path
+from osgeo import gdal
+import sys
+from libtiff import TIFF
+from shutil import copyfile
+from datetime import datetime, timedelta
+import numpy as np
+import time
+
+for filename in os.listdir("s1_tiles"):
+  print(filename)
+  path="s1_tiles/"+filename
+  S1_im=TIFF.open(path)
+  imarray=S1_im.read_image()
+  condition=True
+  for j in range(len(imarray)):
+    for i in range(len(imarray[j])):
+      if(imarray[j][i][0]==0):
+        print("To be deleted because there's a 0")
+        print(str(j)+" "+str(i))
+        condition=False
+        break
+      if(imarray[j][i][1]==-32768 and imarray[j][i][2]==-32768 and imarray[j][i][3]==-32768 and imarray[j][i][4]==-32768):
+        print("To be deleted because all are -32768!!!")
+        print(str(j)+" "+str(i))
+        condition=False
+    if(condition==False):
+      break
+  if(condition==True):
+    print("Not to be deleted!")
