23be4e03a83630462850f76ac44598a1045e77a8
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-09-22 09:55:44 +0300

    Update reprocess.py

--- reprocess.py
+++ reprocess.py
@@ -1,5 +1,7 @@
 import os
+from datetime import datetime
+
 
 for filename in os.listdir("data/S1"):
   dt=os.path.getmtime("data/S1/"+filename)
-  print(dt)
+  print(datetime.utcfromtimestamp(dt).strftime('%Y-%m-%d %H:%M:%S'))
