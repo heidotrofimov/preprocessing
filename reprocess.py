ea6c50636966b7f295c19e95c4e40a372bcc64f8
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-09-22 10:29:36 +0300

    Update reprocess.py

--- reprocess.py
+++ reprocess.py
@@ -30,7 +30,35 @@ for AOI in AOIs:
         dates.append(S1date_str)
   print(AOI)
   print(dates)
- 
+
+print("Test data")  
+
+products=[]
+
+for filename in os.listdir("test_data/S1"):
+  ts=os.path.getmtime("test_data/S1/"+filename)
+  dt=datetime.utcfromtimestamp(ts)
+  if(dt>ref_date):
+    products.append(filename)
+    
+AOIs=[]
+
+for product in products:
+  AOI=product.split("_")[-3]
+  if(AOI not in AOIs):
+    AOIs.append(AOI)
+    
+for AOI in AOIs:
+  dates=[]
+  for product in products:
+    if(AOI in product):
+      S1date_str=product.split("_")[1].split("T")[0]
+      if(S1date_str not in dates):
+        dates.append(S1date_str)
+  print(AOI)
+  print(dates)
+
+
 print("With history")
 
 products=[]
@@ -45,6 +73,33 @@ AOIs=[]
 
 for product in products:
   AOI=product.split("_")[-5]
+  if(AOI not in AOIs):
+    AOIs.append(AOI)
+    
+for AOI in AOIs:
+  dates=[]
+  for product in products:
+    if(AOI in product):
+      S1date_str=product.split("_")[1].split("T")[0]
+      if(S1date_str not in dates):
+        dates.append(S1date_str)
+  print(AOI)
+  print(dates)
+
+  print("Test data")  
+
+products=[]
+
+for filename in os.listdir("test_data/with_history/S1"):
+  ts=os.path.getmtime("test_data/with_history/S1/"+filename)
+  dt=datetime.utcfromtimestamp(ts)
+  if(dt>ref_date):
+    products.append(filename)
+    
+AOIs=[]
+
+for product in products:
+  AOI=product.split("_")[-3]
   if(AOI not in AOIs):
     AOIs.append(AOI)
     
