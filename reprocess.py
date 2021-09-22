c84a5911daf8c8c889b801ea75f756d60b8b2ff3
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-09-22 10:10:26 +0300

    Update reprocess.py

--- reprocess.py
+++ reprocess.py
@@ -4,8 +4,29 @@ from datetime import datetime
 
 ref_date=datetime(2021,9,6,23,59)
 
+print("Without history:")
+
+products=[]
+
 for filename in os.listdir("data/S1"):
   ts=os.path.getmtime("data/S1/"+filename)
   dt=datetime.utcfromtimestamp(ts)
   if(dt>ref_date):
-    print(filename)
+    products.append(filename)
+    
+AOIs=[]
+
+for product in products:
+  AOI=product.split("_")[-3]
+  if(AOI not in AOIs):
+    AOIs.append(AOI)
+    
+for AOI in AOIs:
+  dates=[]
+  for product in products:
+    if(AOI in product):
+      S1date_str=product.split("_")[1].split("T")[0]
+      if(S1date_str not in dates):
+        dates.append(S1date_str)
+  print(AOI)
+  print(dates)
