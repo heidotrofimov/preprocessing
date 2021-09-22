e78088a39fd8f8a706794cf2c4fde6e164ad8587
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-09-22 10:17:40 +0300

    Update reprocess.py

--- reprocess.py
+++ reprocess.py
@@ -30,3 +30,30 @@ for AOI in AOIs:
         dates.append(S1date_str)
   print(AOI)
   print(dates)
+ 
+print("With history")
+
+products=[]
+
+for filename in os.listdir("data/with_history/S1"):
+  ts=os.path.getmtime("data/S1/"+filename)
+  dt=datetime.utcfromtimestamp(ts)
+  if(dt>ref_date):
+    products.append(filename)
+    
+AOIs=[]
+
+for product in products:
+  AOI=product.split("_")[-5]
+  if(AOI not in AOIs):
+    AOIs.append(AOI)
+    
+for AOI in AOIs:
+  dates=[]
+  for product in products:
+    if(AOI in product):
+      S1date_str=product.split("_")[1].split("T")[0]
+      if(S1date_str not in dates):
+        dates.append(S1date_str)
+  print(AOI)
+  print(dates)
