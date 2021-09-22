8463e659c588d4cf87ee43477f8f111977bf06cc
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-09-22 09:57:50 +0300

    Update reprocess.py

--- reprocess.py
+++ reprocess.py
@@ -6,6 +6,6 @@ ref_date=datetime(2021,9,6,23,59)
 
 for filename in os.listdir("data/S1"):
   ts=os.path.getmtime("data/S1/"+filename)
-  dt=datetime.utcfromtimestamp(dt)
+  dt=datetime.utcfromtimestamp(ts)
   if(dt>ref_date):
     print(datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
