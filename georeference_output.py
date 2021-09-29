ebe0de513e34a15330aba207a0a7393c22a73c29
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-09-29 10:18:11 +0300

    Update georeference_output.py

--- georeference_output.py
+++ georeference_output.py
@@ -7,5 +7,5 @@ minx = geoTransform[0]
 maxy = geoTransform[3]
 maxx = minx + geoTransform[1] * data.RasterXSize
 miny = maxy + geoTransform[5] * data.RasterYSize
-print [minx, miny, maxx, maxy]
+print([minx, miny, maxx, maxy])
 data = None
