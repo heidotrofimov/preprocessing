d2b5714c6bd9d67089280e9391433097baa9cad5
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-10-07 13:14:30 +0300

    Update georeference_output.py

--- georeference_output.py
+++ georeference_output.py
@@ -1,8 +1,18 @@
 import gdal
 from gdalconst import GA_ReadOnly
 import os
+import sys
 
-data = gdal.Open('data/S1/S1A_20201008T044450_20CC_colwith_S2A_20201006T094041_T34UDA_19_3.tif', GA_ReadOnly)
+input_tif=sys.argv[1]  #path to input S1 tif
+input_png=sys.argv[2]  #Path to input S2 png. The output PNG will be saved
+
+name_parts=input_png.split("_")
+for p in name_parts:
+  if(p[0]=="T"):
+    EPSG="326"+p[1:2]
+print(EPSG)
+
+data = gdal.Open(input_tif, GA_ReadOnly)
 geoTransform = data.GetGeoTransform()
 minx = geoTransform[0]
 maxy = geoTransform[3]
@@ -10,4 +20,4 @@ maxx = minx + geoTransform[1] * data.RasterXSize
 miny = maxy + geoTransform[5] * data.RasterYSize
 print([minx, miny, maxx, maxy])
 
-os.system("gdal_translate -of Gtiff -a_ullr "+str(minx)+" "+str(maxy)+" "+str(maxx)+" "+str(miny)+" -a_srs EPSG:32634 test_data/S2/S2A_20201006T094041_T34UDA_19_3.png 20201006T094041_T34UDA_19_3.tif")
+os.system("gdal_translate -of Gtiff -a_ullr "+str(minx)+" "+str(maxy)+" "+str(maxx)+" "+str(miny)+" -a_srs EPSG:32634 "+input_png+" "+input_png.replace("png","tif"))
