c5192ca28ca6f314e1cea1460c94e040d1281822
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-10-17 17:21:27 +0300

    Update temp_script.py

--- temp_script.py
+++ temp_script.py
@@ -1,16 +1,16 @@
 
 import os
 
-for S1 in os.listdir("data/S1"):
+for S1 in os.listdir("data/with_history/S1"):
     if("T33UWT" and "F363" in S1):
-        os.system("rm data/S1/"+S1)
+        os.system("rm data/with_history/S1/"+S1)
         S2=S1.split("colwith_")[1].replace("tif","png")
-        for S2f in os.listdir("data/S2"):
+        for S2f in os.listdir("data/with_history/S2"):
             if(S2f==S2):
                 os.system("rm data/S2/"+S2f)           
     if("T33UWT" and "FAA9" in S1):
-        os.system("rm data/S1/"+S1)
+        os.system("rm data/with_history/S1/"+S1)
         S2=S1.split("colwith_")[1].replace("tif","png")
-        for S2f in os.listdir("data/S2"):
+        for S2f in os.listdir("data/with_history/S2"):
             if(S2f==S2):
-                os.system("rm data/S2/"+S2f)
+                os.system("rm data/with_history/S2/"+S2f)
