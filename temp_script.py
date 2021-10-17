aa8ed46f3c6212ae78364ba2b726a5c95c9847bc
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-10-17 17:20:19 +0300

    Update temp_script.py

--- temp_script.py
+++ temp_script.py
@@ -11,6 +11,6 @@ for S1 in os.listdir("data/S1"):
     if("T33UWT" and "FAA9" in S1):
         os.system("rm data/S1/"+S1)
         S2=S1.split("colwith_")[1].replace("tif","png")
-        for S2f in os.listdir("datay/S2"):
+        for S2f in os.listdir("data/S2"):
             if(S2f==S2):
                 os.system("rm data/S2/"+S2f)
