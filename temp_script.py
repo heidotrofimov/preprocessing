5816247ec39cbf41f0f030d7013580beaf79678f
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-10-17 17:07:20 +0300

    Update temp_script.py

--- temp_script.py
+++ temp_script.py
@@ -3,14 +3,14 @@ import os
 
 for S1 in os.listdir("test_data/S1"):
     if("T34UDA" and "CADC" in S1):
-        print("To be deleted:"+S1)
+        os.system("rm test_data/S1/"+S1)
         S2=S1.split("colwith_")[1].replace("tif","png")
         for S2f in os.listdir("test_data/S2"):
             if(S2f==S2):
-                print("To be deleted: "+S2f)            
+                os.system("rm test_data/S2/"+S2f)           
     if("T34UDA" and "D103" in S1):
-        print("To be deleted:"+S1)
+        os.system("rm test_data/S1/"+S1)
         S2=S1.split("colwith_")[1].replace("tif","png")
         for S2f in os.listdir("test_data/S2"):
             if(S2f==S2):
-                print("To be deleted: "+S2f)
+                os.system("rm test_data/S2/"+S2f)
