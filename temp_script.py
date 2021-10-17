b47e49c94b0b69275dd54f55168f531cc99f8f08
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-10-17 17:26:23 +0300

    Update temp_script.py

--- temp_script.py
+++ temp_script.py
@@ -11,3 +11,14 @@ for S2 in os.listdir("data/S2"):
     if(cond==False):
         print("To be deleted: "+S2)
         
+        
+for S2 in os.listdir("data/S2_RGB"):
+    S21=S2.replace("png","tif")
+    cond=False
+    for S1 in os.listdir("data/S1"):
+        S2e=S1.split("colwith_")[1]
+        if(S21==S2e):
+            cond=True
+    if(cond==False):
+        print("To be deleted: "+S2)
+        
