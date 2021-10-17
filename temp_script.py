37a2a9f8106c19993cfc93bfb1a777f740b9fe03
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-10-17 17:03:59 +0300

    Update temp_script.py

--- temp_script.py
+++ temp_script.py
@@ -6,6 +6,6 @@ for S1 in os.listdir("test_data/S1"):
         print("To be deleted:"+S1)
         S2=S1.split("colwith_")[1].replace("tif","png")
         print(S2)
-        for S2f in os.listdir("data/S2"):
+        for S2f in os.listdir("test_data/S2"):
             if(S2f==S2):
                 print("To be deleted: "+S2f)
