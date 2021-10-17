36dc676c97c577f1b38505ec73c2e5ed70fe85f2
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-10-17 17:04:19 +0300

    Update temp_script.py

--- temp_script.py
+++ temp_script.py
@@ -5,7 +5,6 @@ for S1 in os.listdir("test_data/S1"):
     if("T34UDA" and "CADC" in S1):
         print("To be deleted:"+S1)
         S2=S1.split("colwith_")[1].replace("tif","png")
-        print(S2)
         for S2f in os.listdir("test_data/S2"):
             if(S2f==S2):
                 print("To be deleted: "+S2f)
