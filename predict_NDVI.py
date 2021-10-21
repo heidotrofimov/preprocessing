23521427f173e0dcf427f1c97653d23e287ddbca
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-10-21 13:37:24 +0300

    Update predict_NDVI.py

--- predict_NDVI.py
+++ predict_NDVI.py
@@ -5,6 +5,7 @@ S1=sys.argv[1]  #Path to the S1 raster, based on which we want to predict sNDVI
 
 #Linearize raster
 
+'''
 outdir="tmp"
 os.system("mkdir "+outdir)
 filename=os.path.basename(S1).split(".")[0]
@@ -22,6 +23,20 @@ os.system("gdal_calc.py -A "+S1+" --A_band=4 --outfile="+band4+" --type=Float32 
 
 os.system("gdalbuildvrt -q -separate "+prefix+".vrt "+band1+" "+band2+" "+band3+" "+band4)
 os.system("gdal_translate "+prefix+".vrt tmp/"+filename+".tif")
+'''
+
+#Colocate the linearized raster with S2 products:
+
+for S2_product in os.listdir("S2_products":)
+  for folder3 in os.listdir('S2_products/'+S2_product+'/GRANULE/'):
+    if("L2" in folder3):
+      for filename2 in os.listdir('S2_products/'+S2_product+'/GRANULE/'+folder3+'/IMG_DATA/R10m/'):
+        if('B02_10m.jp2' in filename):
+          B02='S2_product/'+S2_product+'/GRANULE/'+folder3+'/IMG_DATA/R10m/'+filename2
+          B02name=filename2
+  targetpath=outdir+"/"+filename2+".dim"
+  os.system("/snap/snap8/bin/gpt collocation.xml -PB02=\""+B02+"\" -PS1=\""+outdir+"/"+S1+"\" -PB02name=\""+B02name+"\" -Ptargetpath=\""+targetpath+"\"")
+
 
 
 
