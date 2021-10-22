750bcc35c706326b9d5f34fa07780a943a0bbb91
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-10-22 10:37:57 +0300

    Update switch_data.py

--- switch_data.py
+++ switch_data.py
@@ -7,30 +7,30 @@ to="biomass_test_data"
 '''
 
 AOI=sys.argv[1]
-from=sys.argv[2]
+fromm=sys.argv[2]
 to=sys.argv[3]
 
-for filename in os.listdir(from+"/S1/"):
+for filename in os.listdir(fromm+"/S1/"):
   if(AOI in filename):
-    os.system("mv "+from+"/S1/"+filename+" "+to+"/S1/")
+    os.system("mv "+fromm+"/S1/"+filename+" "+to+"/S1/")
     
-for filename in os.listdir(from+"/S2/"):
+for filename in os.listdir(fromm+"/S2/"):
   if(AOI in filename):
-    os.system("mv "+from+"/S2/"+filename+" "+to+"/S2/")
+    os.system("mv "+fromm+"/S2/"+filename+" "+to+"/S2/")
     
-for filename in os.listdir(from+"/S2_RGB/"):
+for filename in os.listdir(fromm+"/S2_RGB/"):
   if(AOI in filename):
-    os.system("mv "+from+"/S2_RGB/"+filename+" "+to+"/S2_RGB/")
+    os.system("mv "+fromm+"/S2_RGB/"+filename+" "+to+"/S2_RGB/")
     
     
-for filename in os.listdir(from+"/with_history/S1/"):
+for filename in os.listdir(fromm+"/with_history/S1/"):
   if(AOI in filename):
-    os.system("mv "+from+"/with_history/S1/"+filename+" "+to+"/with_history/S1/")
+    os.system("mv "+fromm+"/with_history/S1/"+filename+" "+to+"/with_history/S1/")
     
-for filename in os.listdir(from+"/with_history/S2/"):
+for filename in os.listdir(fromm+"/with_history/S2/"):
   if(AOI in filename):
-    os.system("mv "+from+"/with_history/S2/"+filename+" "+to+"/with_history/S2/")
+    os.system("mv "+fromm+"/with_history/S2/"+filename+" "+to+"/with_history/S2/")
     
-for filename in os.listdir(from+"/S2_RGB/"):
+for filename in os.listdir(fromm+"/S2_RGB/"):
   if(AOI in filename):
-    os.system("mv "+from+"/with_history/S2_RGB/"+filename+" "+to+"/with_history/S2_RGB/")
+    os.system("mv "+fromm+"/with_history/S2_RGB/"+filename+" "+to+"/with_history/S2_RGB/")
