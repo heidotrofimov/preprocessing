c51136b0776e402e9668afca846ebef1e5ec3efb
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-11-01 15:16:10 +0200

    Update collect_extra_historical_data.py

--- collect_extra_historical_data.py
+++ collect_extra_historical_data.py
@@ -52,7 +52,7 @@ for S2 in os.listdir("data/with_history/S2/"):
           S1_history="data/S1/"+name
           ref=days(S1_str,history_str)
   if(len(S1_history)>2 and len(S1_target)>2):
-    new_name=S1_target.split("_colwith")[0]+"_"+S1_history.split("_colwith")[0]+"_colwith_"+S1_target.split("colwith_")[1]
+    new_name=S1_target.split("_colwith")[0]+"_"+S1_history.split("/")[-1].split("_colwith")[0]+"_colwith_"+S1_target.split("colwith_")[1]
     os.system("cp data/with_history/S2/"+S2+" data/extra_historical/S2/")
     os.system("gdal_merge.py -ot Float32 -of GTiff -separate -o data/extra_historical/S1/"+new_name+" data/with_history/S1/"+S1_target+" "+S1_history)
     print("")
