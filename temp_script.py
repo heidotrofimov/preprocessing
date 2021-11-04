c2e3296fb80e85ac58b0eb6b19dbe6b572392f20
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-11-04 19:08:12 +0200

    Update temp_script.py

--- temp_script.py
+++ temp_script.py
@@ -3,5 +3,5 @@ import os
 for product in os.listdir("checked_products"):
     for filename in os.listdir("checked_products/"+product):
         newname=filename.split("_")[-2]+"_"+filename.split("_")[-1]
-        print(newname)
+        os.system("mv checked_products/"+product+"/"+filename+" checked_products/"+product+"/"+newname)
     
