8caca051795ab404e6789af6368713606fd60371
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2022-01-03 16:05:27 +0200

    Update add_bands.py

--- add_bands.py
+++ add_bands.py
@@ -1,5 +1,11 @@
 import os
 
+f=open("../find_products/login.txt","r")
+lines=f.readlines()
+username=lines[0].rstrip()
+password=lines[1].rstrip()
+f.close()
+
 S2s=[]
 
 def S21(str):
@@ -20,4 +26,34 @@ for S1 in os.listdir("/home/users/biomass/extra_historical/S1"):
     if(S2_2 not in S2s):
       S2s.append(S2_2)
       
-print(S2s)
+      
+def download_xml(product, out_path):
+    user_name = username
+    user_password = password
+    scihub_url = "https://scihub.copernicus.eu/dhus/search?q="
+    command = "wget --no-verbose --no-check-certificate --user={user} --password={pwd} --output-document={out}"\
+                  " {url}".format(user=user_name, pwd=user_password, out=out_path, url="\""+scihub_url + product+"&rows=100\"")
+
+    print("Downloading product as " + command)
+    os.system(command)
+    time.sleep(1.5)  # scihub does not allow too frequent queries; therefore wait a bit before a new query
+
+def read_xml(out_path):
+    current_list=[]
+    txt = Path(out_path).read_text()
+    products=txt.split("<title>")
+    for i in range(2,len(products)):
+        product=products[i].split("</title>")[0]
+        product_list.append(product)
+    return current_list
+
+products=[]  
+
+for S2 in S2s:
+  download_xml(S2,"scihub.xml")
+  product=read_xml("scihub.xml")[0]
+  products.append(product)
+  os.system("rm scihub.xml")
+  
+print(products)
+  
