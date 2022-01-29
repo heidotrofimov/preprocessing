c2e0e80245bf545b069f25679c1a64b33b53a890
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2022-01-29 15:09:52 +0200

    Update validate.py

--- validate.py
+++ validate.py
@@ -1,8 +1,13 @@
 import os
-from PIL import Image
-import numpy as np
+import gdal
+from gdalconst import GA_ReadOnly
 
 for tiff in os.listdir("/home/users/est_data_with_extra_bands/S1"):
+  '''
   im=Image.open("/home/users/est_data_with_extra_bands/S1/"+tiff)
   imarray = np.array(im)
   print(imarray.shape)
+  '''
+  src_ds = gdal.Open(/home/users/est_data_with_extra_bands/S1/"+tiff)
+  if src_ds is not None: 
+    print ("band count: " + str(src_ds.RasterCount))
