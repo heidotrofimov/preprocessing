d75bb837e182fa8667a3234e4a269372fec716f3
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2022-01-29 15:41:06 +0200

    Update validate.py

--- validate.py
+++ validate.py
@@ -8,6 +8,7 @@ for tiff in os.listdir("/home/users/est_data_with_extra_bands/S1"):
   imarray = np.array(im)
   print(imarray.shape)
   '''
+  print(tiff)
   src_ds = gdal.Open("/home/users/est_data_with_extra_bands/S1/"+tiff)
   if src_ds is not None: 
     print ("band count: " + str(src_ds.RasterCount))
