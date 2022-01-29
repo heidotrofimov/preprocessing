0d0fbfe1e547d43abe8974808ea2a5160e493a6c
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2022-01-29 15:08:23 +0200

    Update validate.py

--- validate.py
+++ validate.py
@@ -2,7 +2,7 @@ import os
 from PIL import Image
 import numpy as np
 
-for tiff in os.listdir("/home/users/est_data_with_extra_bands"):
-  im=Image.open("/home/users/est_data_with_extra_bands/"+tiff)
+for tiff in os.listdir("/home/users/est_data_with_extra_bands/S1"):
+  im=Image.open("/home/users/est_data_with_extra_bands/S1/"+tiff)
   imarray = np.array(im)
   print(imarray.shape)
