609beda748b5d00e98dde19de6a24de85fa31d56
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2022-01-29 15:04:08 +0200

    Update validate.py

--- validate.py
+++ validate.py
@@ -4,5 +4,5 @@ import numpy as np
 
 for tiff in os.listdir("extra_bands_tif"):
   im=Image.open("extra_bands_tif/"+tiff)
-  imarray = numpy.array(im)
+  imarray = np.array(im)
   print(imarray.shape)
