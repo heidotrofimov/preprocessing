8993c670ccdb9115bd3db31a8947f8cadc2e7c5c
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2022-02-12 17:06:49 +0200

    Update validate.py

--- validate.py
+++ validate.py
@@ -2,12 +2,12 @@ import os
 import gdal
 from gdalconst import GA_ReadOnly
 
-for tiff in os.listdir("new_data"):
+for tiff in os.listdir("/home/users/est_data_with_extra_bands/S1"):
   '''
   im=Image.open("/home/users/est_data_with_extra_bands/S1/"+tiff)
   imarray = np.array(im)
   print(imarray.shape)
-  '''
+  
   #print(tiff)
   src_ds = gdal.Open("new_data/"+tiff)
   x=src_ds.RasterXSize
@@ -15,4 +15,8 @@ for tiff in os.listdir("new_data"):
   result=str(src_ds.RasterCount)+" "+str(x)+" "+str(y)
   if(result!="18 512 512"):
     print(tiff+" "+result)
-
+  '''
+  src_ds = gdal.Open("/home/users/est_data_with_extra_bands/S1/"+tiff)
+  band = src_ds.GetRasterBand(0)
+  print(tiff)
+  print(band)
