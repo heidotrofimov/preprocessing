7fb7214bcef69523e0f1b427ab69352cf03cc399
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2022-02-16 16:51:11 +0200

    Create check_tiles.py

---
+++ check_tiles.py
@@ -0,0 +1,16 @@
+import os
+import os.path
+from osgeo import gdal
+import sys
+from libtiff import TIFF
+from shutil import copyfile
+from datetime import datetime, timedelta
+import numpy as np
+import time
+
+for filename in os.listdir("s1_tiles"):
+      print(filename)
+      path="s1_tiles/"+filename
+      S1_im=TIFF.open(path)
+      imarray=S1_im.read_image()
+      print(imarray)
