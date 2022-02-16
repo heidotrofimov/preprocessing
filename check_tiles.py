d5c822a019485f7ecec0161d2002070501d913b8
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2022-02-16 17:45:09 +0200

    Update check_tiles.py

--- check_tiles.py
+++ check_tiles.py
@@ -9,9 +9,10 @@ import numpy as np
 import time
 
 for filename in os.listdir("s1_tiles"):
+      os.system("gdal_translate -b 2 -b 3 -b 4 -b 5 s1_tiles/"+filename+" s1_tiles/"+filename)
       print(filename)
       path="s1_tiles/"+filename
       S1_im=TIFF.open(path)
       a=S1_im.read_image()
-      if(np.all(a==0)==False):
+      if(np.all(a==0)==False and np.all(a==-32768)==False):
             print(a)
