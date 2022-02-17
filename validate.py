647bbbc85290ab4fc790663be94e34a905cf84c4
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2022-02-17 16:56:02 +0200

    Update validate.py

--- validate.py
+++ validate.py
@@ -2,7 +2,7 @@ import os
 import gdal
 from gdalconst import GA_ReadOnly
 from libtiff import TIFF
-
+'''
 inputdir="collocated_tifs"
 for filename in os.listdir(inputdir):
     print(filename)
@@ -14,23 +14,22 @@ for filename in os.listdir(inputdir):
     except exception as e:
         print(e)
 
+'''
 
-
-for tiff in os.listdir("new_data"):
-  pass
-  '''
-  im=Image.open("new_data/"+tiff)
-  imarray = np.array(im)
-  print(imarray.shape)
+for tiff in os.listdir("T35VLH_data"):
+  #im=Image.open("T35VLH_data/"+tiff)
+  #imarray = np.array(im)
+  #print(imarray.shape)
   
   #print(tiff)
-  src_ds = gdal.Open("new_data/"+tiff)
+  src_ds = gdal.Open("T35VLH_data/"+tiff)
   x=src_ds.RasterXSize
   y=src_ds.RasterYSize
   result=str(src_ds.RasterCount)+" "+str(x)+" "+str(y)
-  if(result!="18 512 512"):
-    print(tiff+" "+result)
- 
+  print(result)
+  #if(result!="18 512 512"):
+  #  print(tiff+" "+result)
+  '''
   if("T35VMF" in tiff and "_18_18.tif" in tiff):
     print(tiff)
     src_ds = gdal.Open("/home/users/est_data_with_extra_bands/S1/"+tiff)
